9cc5fa7c55f744175610469ab17af4b2f4e805c9
Victor Zamfir <zamfir.victor@gmail.com>
2016-05-04 21:04:35 +0300

    decorating the request and reply interfaces

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import { createEventDispatcher } from 'octobus.js';
 import pkg from '../package.json';
 
+const internals = {
+  handlers: {},
+  replies: {},
+};
+
 export function register(server, options, next) {
   const eventDispatcher = createEventDispatcher(options);
 
   server.expose('eventDispatcher', eventDispatcher);
 
   server.decorate('server', 'eventDispatcher', eventDispatcher);
+  server.decorate('request', 'eventDispatcher', eventDispatcher);
+  server.decorate('reply', 'dispatch', internals.replies.dispatch);
+  server.handler('dispatch', internals.handlers.dispatch);
 
   next();
 }
@@ -14,4 +22,21 @@ export function register(server, options, next) {
 register.attributes = {
   pkg,
   dependencies: [],
+};
+
+internals.replies.dispatch = function dispatch(event, params = {}) {
+  const { request, response } = this;
+  const { eventDispatcher } = request;
+
+  response(eventDispatcher.dispatch(event, params));
+};
+
+internals.handlers.dispatch = (route, options) => (request, reply) => {
+  const event = options;
+
+  return reply.dispatch(event, {
+    ...request.params,
+    ...request.query,
+    ...request.payload,
+  });
 };
